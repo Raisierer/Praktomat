3fcee57f2f1f70a3ed8ac3f053574bcf3ade3d67
Adrian Raiser <adrian.raiser10@web.de>
2019-10-23 09:09:56 +0200

    changed settings to uni stuttgart

--- src/settings/local.py
+++ src/settings/local.py
@@ -1,9 +1,9 @@
 # Settings for deployment
 
-# These settings are KIT-specific and derive some parts of the settings
+# These settings are specific for the University of Stuttgart and derive some parts of the settings
 # from the directory name.
 #
-# If you are not deploying on praktomat.cs.kit.edu you need to rewrite this file.
+# If you are not deploying on praktomat.ils.uni-stuttgart.de you need to rewrite this file.
 
 from os.path import join, dirname, basename
 import re
@@ -12,64 +12,20 @@ PRAKTOMAT_PATH = dirname(dirname(dirname(__file__)))
 
 PRAKTOMAT_ID = basename(dirname(PRAKTOMAT_PATH))
 
-match = re.match(r'''
-    (?:praktomat_)?
-    (?P<algo1>algo1_)?
-    (?P<cram>cram_)?
-    (?P<birap>birap_)?
-    (?P<tba>tba_)?
-    (?P<mlfds>mlfds_)?
-    (?P<pp>pp_)?
-    (?P<iimb>iimb_)?
-    (?P<year>\d+)_
-    (?P<semester>WS|SS)
-    (?P<abschluss>_Abschluss)?
-    (?P<mirror>_Mirror)?
-    ''', PRAKTOMAT_ID, flags=re.VERBOSE)
+DEBUG = False
 
-if match:
-    if match.group('algo1') is not None:
-        SITE_NAME = 'Algorithmen I '
-    elif match.group('cram') is not None:
-        SITE_NAME = 'CRAM '
-    elif match.group('birap') is not None:
-        SITE_NAME = 'BIRAP '
-    elif match.group('mlfds') is not None:
-        SITE_NAME = 'MLFDS '
-    elif match.group('tba') is not None:
-        SITE_NAME = 'Theorembeweiser '
-    elif match.group('pp') is not None:
-        SITE_NAME = 'Programmierparadigmen '
-    elif match.group('iimb') is not None:
-        SITE_NAME = 'Informatik im Maschinenbau '
-    else:
-        SITE_NAME = 'Programmieren '
-
-    if match.group('abschluss'):
-        SITE_NAME += "Abschlussaufgaben "
-
-    year = int(match.group('year'))
-    if match.group('semester') == "WS":
-        SITE_NAME += "Wintersemester %d/%d" % (year, year+1)
-    else:
-        SITE_NAME += "Sommersemester %d" % year
-
-    if match.group('mirror') is not None:
-        SITE_NAME += " (Mirror)"
-        MIRROR = True
-    else:
-        MIRROR = False
-
-else:
-    raise NotImplementedError("Autoconfig for PRAKTOMAT_ID %s not possible", PRAKTOMAT_ID)
+MIRROR = False
 
 
 # The URL where this site is reachable. 'http://localhost:8000/' in case of the
 # development server.
-BASE_HOST = 'https://praktomat.cs.kit.edu'
+BASE_HOST = 'https://praktomat.ils.uni-stuttgart.de'
 BASE_PATH = '/' + PRAKTOMAT_ID + '/'
 
-ALLOWED_HOSTS = [ 'praktomat.cs.kit.edu', ]
+SITE_NAME = 'ILS Praktomat'
+site_name = 'ILS Praktomat'
+
+ALLOWED_HOSTS = ['localhost', 'praktomat.ils.uni-stuttgart.de']
 
 # URL to use when referring to static files.
 STATIC_URL = BASE_PATH + 'static/'
@@ -81,21 +37,6 @@ TEST_MAXLOGSIZE=512
 TEST_MAXFILESIZE=512
 
 TEST_TIMEOUT=180
-
-if "cram" in PRAKTOMAT_ID:
-  TEST_TIMEOUT=600
-  TEST_MAXMEM=200
-
-if "birap" in PRAKTOMAT_ID:
-  TEST_TIMEOUT=600
-
-if "tba" in PRAKTOMAT_ID:
-  TEST_TIMEOUT=600
-
-if "Programmieren" in SITE_NAME:
-  # Rating overview needs one POST parameter per student
-  # and the default value (1000) might be too low
-  DATA_UPLOAD_MAX_NUMBER_FIELDS = 2000
 
 # Absolute path to the directory that shall hold all uploaded files as well as
 # files created at runtime.
@@ -109,46 +50,44 @@ else:
     SANDBOX_DIR = join('/srv/praktomat/sandbox/', PRAKTOMAT_ID)
 
 ADMINS = [
-  ('Praktomat', 'praktomat@ipd.info.uni-karlsruhe.de')
+  ('Praktomat', 'praktomat@ils.uni-stuttgart.de')
 ]
 
-SERVER_EMAIL = 'praktomat@i44vm3.info.uni-karlsruhe.de'
+# EMail settings
 
+SERVER_EMAIL = 'praktomat@ils.uni-stuttgart.de'
 
-if MIRROR:
-    EMAIL_BACKEND = 'django.core.mail.backends.filebased.EmailBackend'
-    EMAIL_FILE_PATH = join(UPLOAD_ROOT, "sent-mails")
-else:
-    EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-    EMAIL_HOST = "localhost"
-    EMAIL_PORT = 25
+EMAIL_BACKEND = 'django.core.mail.backend.smtp.EmailBackend'
 
-DEFAULT_FROM_EMAIL = "praktomat@ipd.info.uni-karlsruhe.de"
+# import email settings from file located in Praktomat parent folder
+from ....email_settings import *
 
-DEBUG = MIRROR
+DEFAULT_FROM_EMAIL = "praktomat@ils.uni-stuttgart.de"
 
 DATABASES = {
     'default': {
             'ENGINE': 'django.db.backends.postgresql_psycopg2',
-            'NAME':   PRAKTOMAT_ID,
+            # needs to be created first with sudo -u postgres createdb -O praktomat praktomat_<praktomat_id>
+            'NAME':   'praktomat_' + PRAKTOMAT_ID, 
     }
 }
 
 # Private key used to sign uploded solution files in submission confirmation email
-PRIVATE_KEY = '/srv/praktomat/mailsign/signer_key.pem'
-CERTIFICATE = '/srv/praktomat/mailsign/signer.pem'
+# PRIVATE_KEY = '/srv/praktomat/mailsign/signer_key.pem'
+PRIVATE_KEY = '/etc/ssl/private/praktomat.key'
+#CERTIFICATE = '/srv/praktomat/mailsign/signer.pem'
 
 # Enable Shibboleth:
-SHIB_ENABLED = True
-REGISTRATION_POSSIBLE = False
+SHIB_ENABLED = False
+REGISTRATION_POSSIBLE = True
 
-SYSADMIN_MOTD_URL = "https://praktomat.cs.kit.edu/sysadmin_motd.html"
+# SYSADMIN_MOTD_URL = "https://praktomat.cs.kit.edu/sysadmin_motd.html"
 
 # Use a dedicated user to test submissions
-USEPRAKTOMATTESTER = False
+USEPRAKTOMATTESTER = True
 
 # Use docker to test submission
-USESAFEDOCKER = True
+USESAFEDOCKER = False
 
 # Various extra files and versions
 CHECKSTYLEALLJAR = '/srv/praktomat/contrib/checkstyle-5.7-all.jar'
@@ -158,9 +97,6 @@ JPLAGJAR = '/srv/praktomat/contrib/jplag.jar'
 
 # Our VM has 4 cores, so lets try to use them
 NUMBER_OF_TASKS_TO_BE_CHECKED_IN_PARALLEL = 6
-# But not with Isabelle, which is memory bound
-if match.group('tba') is not None:
-    NUMBER_OF_TASKS_TO_BE_CHECKED_IN_PARALLEL = 1
 
 # Finally load defaults for missing settings.
 from . import defaults
