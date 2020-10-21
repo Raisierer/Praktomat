f837b9b26a0db1a3e6a84107ffc6c863c386ebbd
Adrian Raiser <adrian.raiser10@web.de>
2020-10-21 02:08:04 +0200

    set deployment settings for docker-compose usage

--- src/settings/local.py
+++ src/settings/local.py
@@ -5,12 +5,15 @@
 #
 # If you are not deploying on praktomat.ils.uni-stuttgart.de you need to rewrite this file.
 
+from . import defaults
+from .configuration import load_configuration
+import os
 from os.path import join, dirname, basename
 import re
 
-PRAKTOMAT_PATH = dirname(dirname(dirname(__file__)))
+PRAKTOMAT_PATH = '/server/Praktomat'
 
-PRAKTOMAT_ID = basename(dirname(PRAKTOMAT_PATH))
+PRAKTOMAT_ID = os.environ['ID']
 
 DEBUG = False
 
@@ -19,11 +22,11 @@ MIRROR = False
 
 # The URL where this site is reachable. 'http://localhost:8000/' in case of the
 # development server.
-BASE_HOST = 'https://praktomat.ils.uni-stuttgart.de'
+BASE_HOST = os.environ['BASE_HOST']
 BASE_PATH = '/' + PRAKTOMAT_ID + '/'
 
-SITE_NAME = 'ILS Praktomat'
-site_name = 'ILS Praktomat'
+SITE_NAME = os.environ['SITE_NAME']
+site_name = os.environ['SITE_NAME']
 
 ALLOWED_HOSTS = ['localhost', 'praktomat.ils.uni-stuttgart.de']
 
@@ -32,11 +35,11 @@ STATIC_URL = BASE_PATH + 'static/'
 
 STATIC_ROOT = join(dirname(PRAKTOMAT_PATH), "static")
 
-TEST_MAXLOGSIZE=512
+TEST_MAXLOGSIZE = 512
 
-TEST_MAXFILESIZE=512
+TEST_MAXFILESIZE = 512
 
-TEST_TIMEOUT=180
+TEST_TIMEOUT = 180
 
 # Absolute path to the directory that shall hold all uploaded files as well as
 # files created at runtime.
@@ -50,7 +53,7 @@ else:
     SANDBOX_DIR = join('/srv/praktomat/sandbox/', PRAKTOMAT_ID)
 
 ADMINS = [
-  ('Praktomat', 'praktomat@ils.uni-stuttgart.de')
+    ('Praktomat', 'praktomat@ils.uni-stuttgart.de')
 ]
 
 # EMail settings
@@ -59,9 +62,21 @@ SERVER_EMAIL = 'praktomat@ils.uni-stuttgart.de'
 
 EMAIL_BACKEND = 'django.core.mail.backend.smtp.EmailBackend'
 
-# import email settings from file located in Praktomat parent folder
-from .configuration import load_configuration
-load_configuration()
+EMAIL_HOST_USER = os.environ['EMAIL_USERNAME']
+EMAIL_HOST_PASSWORD = os.environ['EMAIL_PASSWORD']
+
+# Database settings
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': PRAKTOMAT_ID,
+        'USER': 'postgres',
+        'PASSWORD': os.environ['POSTGRES_PASSWORD'],
+        'HOST': 'db',
+        'PORT': 5432,
+    }
+}
 
 
 # Private key used to sign uploded solution files in submission confirmation email
@@ -82,14 +97,13 @@ USEPRAKTOMATTESTER = True
 USESAFEDOCKER = False
 
 # Various extra files and versions
-CHECKSTYLEALLJAR = '/srv/praktomat/contrib/checkstyle-5.7-all.jar'
-JPLAGJAR = '/srv/praktomat/contrib/jplag.jar'
+#CHECKSTYLEALLJAR = '/srv/praktomat/contrib/checkstyle-5.7-all.jar'
+#JPLAGJAR = '/srv/praktomat/contrib/jplag.jar'
 #JAVA_BINARY = 'javac-sun-1.7'
 #JVM = 'java-sun-1.7'
 
 # Our VM has 4 cores, so lets try to use them
-NUMBER_OF_TASKS_TO_BE_CHECKED_IN_PARALLEL = 6
+NUMBER_OF_TASKS_TO_BE_CHECKED_IN_PARALLEL = 4
 
 # Finally load defaults for missing settings.
-from . import defaults
 defaults.load_defaults(globals())
